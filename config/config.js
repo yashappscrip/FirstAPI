0a3398ab58b2109a020223e6bdd672e7c7240c0c
nikunj <faldu@mobifyi.com>
2019-09-11 11:44:00 +0530

    Added config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+let config={};
+
+config.port=4000;
+config.allowedCorsOrigin = "*";
+config.env="dev";
+config.db={
+    url:"mongodb+srv://yash:110198@cluster0-xowlj.mongodb.net/Demno?retryWrites=true&w=majority"
+};
